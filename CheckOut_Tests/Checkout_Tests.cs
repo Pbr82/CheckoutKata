54e8097ac099aea288bd8fd48924bb6d469175ea
Peter Rowlands <prowlands@autoscribeinformatics.com>
2024-06-05 22:09:58 +0100

    Write tests

--- CheckOut_Tests/Checkout_Tests.cs
+++ CheckOut_Tests/Checkout_Tests.cs
@@ -1,11 +1,58 @@
+using CheckoutKata;
+using Moq;
+using System.Collections;
+
 namespace CheckoutKata_Tests
 {
     public class Checkout_Tests
     {
-        [Test]
-        public void Test1()
+        /// <summary>
+        /// Ensure the Checkout defers the calculation of total price to the rules
+        /// </summary>
+        /// <param name="expected"></param>
+        [TestCase(-1)]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        public void Checkout_Returns_Calculated_Value_From_Rules(int expected)
         {
-            Assert.Pass();
+            var rules = new Mock<IRules>();
+            rules.Setup(r => r.Calculate(It.IsAny<IEnumerable<string>>())).Returns(expected);
+            var checkout = new Checkout(rules.Object);
+            Assert.That(checkout.GetTotalPrice(), Is.EqualTo(expected));
+        }
+
+        /// <summary>
+        /// Ensure all items scanned by the checkout are passed to the price Calulator (IRules)
+        /// </summary>
+        /// <param name="items"></param>
+        [TestCaseSource(nameof(ScannedItems))]
+        public void Checkout_Passes_Scanned_Items_To_Rules(IEnumerable<string> items)
+        {
+            var rules = new Mock<IRules>();
+            IEnumerable<string> itemParams = [];
+            rules.Setup(r => r.Calculate(It.IsAny<IEnumerable<string>>())).Callback<IEnumerable<string>>(e => itemParams = e);
+
+            var checkout = new Checkout(rules.Object);
+
+            foreach (var item in items)
+                checkout.Scan(item);
+            
+            checkout.GetTotalPrice();
+
+            CollectionAssert.AreEquivalent(items, itemParams);
+        }
+
+        private static IEnumerable ScannedItems
+        { 
+            get 
+            { 
+                yield return new List<string>(){"A"};
+                yield return new List<string>(){"A", "B"};
+                yield return new List<string>(){"A", "B", "A", "C"};
+                yield return new List<string>(){"A", "A", "A", "B"};
+                yield return new List<string>(){"A", "B", "G", "Z", "XXX"};
+            }
         }
     }
 }
--- CheckOut_Tests/DefaultPricingRules_Tests.cs
+++ CheckOut_Tests/DefaultPricingRules_Tests.cs
@@ -1,6 +1,55 @@
-﻿namespace CheckoutKata_Tests
+﻿using CheckoutKata;
+using System.Collections;
+
+namespace CheckoutKata_Tests
 {
     internal class DefaultPricingRules_Tests
     {
+
+        [TestCaseSource(nameof(TestData))]
+        public int Calculate_Returns_The_Correct_Price(IEnumerable<string> items)
+        {
+            var rules = new DefaultPricingRules();
+            return rules.Calculate(items);
+        }
+
+        private static IEnumerable TestData
+        { 
+            get 
+            { 
+                // single items
+                yield return new TestCaseData(new List<string>(){"A"}).Returns(50);
+                yield return new TestCaseData(new List<string>(){"B"}).Returns(30);
+                yield return new TestCaseData(new List<string>(){"C"}).Returns(20);
+                yield return new TestCaseData(new List<string>(){"D"}).Returns(15);
+                
+                // 2 items
+                yield return new TestCaseData(new List<string>(){"A", "A"}).Returns(100);
+                yield return new TestCaseData(new List<string>(){"B", "B"}).Returns(45); // discounted
+                yield return new TestCaseData(new List<string>(){"C", "C"}).Returns(40); 
+                yield return new TestCaseData(new List<string>(){"D", "D"}).Returns(30);
+                
+                // 3 items
+                yield return new TestCaseData(new List<string>(){"A", "A", "A"}).Returns(130); // discounted
+                yield return new TestCaseData(new List<string>(){"B", "B", "B"}).Returns(75); // discounted (1 full price)
+                yield return new TestCaseData(new List<string>(){"C", "C", "C"}).Returns(60);
+                yield return new TestCaseData(new List<string>(){"D", "D", "D"}).Returns(45);
+
+                // discounted items
+                yield return new TestCaseData(new List<string>(){"A", "A", "A", "A", "A", "A"}).Returns(260); // double discounted
+                yield return new TestCaseData(new List<string>(){"B", "B", "B", "B"}).Returns(90); // double discounted
+
+                // out of order items
+                // 6 * A = 260
+                // 3 * B =  75
+                // 4 * C =  80
+                // 6 * D =  90   
+                yield return new TestCaseData(new List<string>()
+                    {"A", "D", "B", "A", "C", 
+                     "D", "D", "A", "C", "B",
+                     "A", "D", "B", "A", "D",
+                     "A", "C", "C", "D"}).Returns(505);
+            }
+        }
     }
 }
