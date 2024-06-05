7fb78a33bfb5aebafe12162ae47af0f59fa91676
Peter Rowlands <prowlands@autoscribeinformatics.com>
2024-06-05 20:17:10 +0100

    Add project files.

---
+++ CheckOut_Tests/Checkout_Tests.cs
@@ -0,0 +1,16 @@
+namespace CheckOut_Tests
+{
+    public class Checkout_Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
